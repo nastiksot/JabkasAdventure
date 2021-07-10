66d7ce22648f128a37809579579a3a7609374311
Anastasiia <nastiksot02@gmail.com>
2021-07-10 15:12:45 -0700

    Refactor | Add bookshelf

--- Assets/ui/Games/MarioLevel/LibraryBook.cs
+++ Assets/ui/Games/MarioLevel/LibraryBook.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Services.Constants;
 using UI.Base;
@@ -7,44 +8,40 @@ namespace UI.Games.MarioLevel
 {
     public class LibraryBook : BaseMono
     {
+        [SerializeField] private SpriteRenderer blockSprite;  
+        [SerializeField] private Sprite emptyShelfSprite;
+        [Space (6f), Header("Prefab")][SerializeField] private GameObject CVSheet;
+
+        //private AudioSource hitBlock;
         private bool isContainSheet = true;
-        [SerializeField] private Sprite[] shelfSprite;
-        [SerializeField] private GameObject CVSheet;
-    
-        //public AudioSource hitBlock;
         private bool isShelfClosed = true;
+ 
         private void OnCollisionEnter2D(Collision2D col)
-        {
-            //compare collision BonusBlock and Player
+        { 
             if (col.collider.bounds.max.y < transform.position.y &&
                 col.collider.bounds.min.x < transform.position.x + 0.3f &&
                 col.collider.bounds.min.x < transform.position.x - 0.3f &&
                 col.gameObject.CompareTag(Tags.PLAYER_TAG))
             {
-                //check of condition BonusBlock(empty or full) 
                 if (isContainSheet)
                 {
-                    //hitBlock.Play();
-                    gameObject.GetComponent<SpriteRenderer>().sprite = shelfSprite[0];
+                    //TODO: init sound hit block; 
                     isContainSheet = false;
                 }
 
                 if (isContainSheet) return;
-                StartCoroutine(WaitForHalfASecond());
-                // GetComponent<Animator>().SetBool("isEmpty", true);
-                gameObject.GetComponent<SpriteRenderer>().sprite = shelfSprite[1];
+                StartCoroutine(WaitForHalfASecond());  
+                blockSprite.sprite = emptyShelfSprite;
             }
         }
 
-        IEnumerator WaitForHalfASecond()
+        private IEnumerator WaitForHalfASecond()
         {
             yield return new WaitForSeconds(0.17f);
-            if (isShelfClosed)
-            {
-                //drop list just once
-                Instantiate(CVSheet, transform.position + Vector3.up, Quaternion.identity);
-                isShelfClosed = false;
-            }
+            if (!isShelfClosed) yield break;
+            Instantiate(CVSheet, transform.position + Vector3.up, Quaternion.identity);
+            isShelfClosed = false;
         }
+ 
     }
 }
