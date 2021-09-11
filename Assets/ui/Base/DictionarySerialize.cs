b0237fe5f55691440bd585c8d3d779bb351f0a1e
Anastasiia <nastiksot02@gmail.com>
2021-09-11 20:44:34 +0300

    JA-28 | Added DoTween

---
+++ Assets/ui/Base/DictionarySerialize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UI.Base
+{
+    [Serializable]
+    public class DictionarySerialize<TKey, TValue>
+    {
+        public TKey Key = default;
+        public TValue Value = default;
+    }
+
+    [Serializable]
+    public class MultiValueDictionary<TKey, TValue> : Dictionary<TKey, List<TValue>>
+    {
+        public TKey Key = default;
+        public TValue Value = default;
+
+        public void Add(TKey key, TValue value)
+        {
+            List<TValue> values;
+            if (!TryGetValue(key, out values))
+            {
+                values = new List<TValue>();
+                this.Add(key, values);
+            }
+
+            values.Add(value);
+        }
+    }
+
+    [Serializable]
+    public class DictionarySerialize< TKey, TValue1, TValue2>
+    {
+        public TKey Key = default;
+        public TValue1 Value1 = default;
+        public TValue2 Value2 = default;
+    }
+}
---
+++ Assets/ui/Games/Menus/MainMenuAnimation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UI.Base;
+using UnityEngine;
+
+namespace UI.Games.Menus
+{
+    public class MainMenuAnimation : MonoBehaviour
+    {
+        [SerializeField] private List<DictionarySerialize<RectTransform, List<int>>> transformList;
+        [SerializeField] private float delay = 15f;
+
+        private void Start()
+        {
+            ResetToDefaultPosition();
+            StartCoroutine(StartAnimation());
+        }
+
+        private void ResetToDefaultPosition()
+        {
+            foreach (var imageTransform in transformList)
+            {
+                imageTransform.Key.localPosition =
+                    new Vector2(imageTransform.Key.localPosition.x, imageTransform.Value[0]);
+            }
+        }
+
+        private IEnumerator StartAnimation()
+        {
+            while (true)
+            {
+                foreach (var imageTransform in transformList)
+                {
+                    for (var i = 0; i < imageTransform.Value.Count; i++)
+                    {
+                        imageTransform.Key.DOLocalMoveY(imageTransform.Value[i], delay);
+                        if (i != 1) continue;
+                        yield return new WaitForSeconds(delay + 1);
+                    }
+
+                    for (var i = imageTransform.Value.Count - 1; i >= 0; i--)
+                    {
+                        imageTransform.Key.DOLocalMoveY(imageTransform.Value[i], delay);
+                        if (i == 2)
+                        {
+                            yield return new WaitForSeconds(delay - 1);
+                        }
+                    } 
+                    yield return new WaitForSeconds(delay);
+                }
+                
+            }
+        }
+    }
+}
